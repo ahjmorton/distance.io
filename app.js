7659c45c104151b43ff0a375c113bd001b6dcb47
Andrew Morton <ahjmorton@gmail.com>
2015-08-29 16:41:19 +0100

    Basic working version of application created

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 'use strict';
 
+var redis = require('redis'),
+    client = redis.createClient();
+
+client.on('error', function(err) {
+   console.log('Redis Error ' + err);
+});
+
 /*
  * Express Dependencies
  */
-var express = require('express');
-var app = express();
+var express = require('express'),
+    expressValidator = require('express-validator'),
+    app = express();
+
 var port = 3000;
 
 /*
@@ -15,6 +24,8 @@ var hbs;
 
 // For gzip compression
 app.use(express.compress());
+app.use(express.bodyParser());
+app.use(expressValidator());
 
 /*
  * Config for Production and Development
@@ -51,8 +62,6 @@ if (process.env.NODE_ENV === 'production') {
 // Set Handlebars
 app.set('view engine', 'handlebars');
 
-
-
 /*
  * Routes
  */
@@ -61,6 +70,31 @@ app.get('/', function(request, response, next) {
     response.render('index');
 });
 
+
+/*
+ * Get entry 
+ */
+app.get('/distance', function(request, response, next) {
+    request.checkQuery('seconds', 'Require number of seconds').isDecimal();
+
+    var errors = request.validationErrors();
+    if(errors) {
+      next(errors);
+      return;
+    }
+
+    var seconds = request.query.seconds;
+
+    client.zrangebyscore(['times', seconds , '+inf', 'limit', 0, 1], 
+      function(err, result) {
+        if(err) {
+          next(err);
+        } else {
+          response.json(result);
+        }
+      }
+    );
+});
 
 /*
  * Start it up
